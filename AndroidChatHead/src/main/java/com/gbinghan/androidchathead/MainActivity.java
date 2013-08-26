7ca6de5ac287a815c08e9493dd2ce769aa20f9be
Bing Han, Goh <stiucsib86@hotmail.com>
2013-08-26 23:47:37 +0800

    Added button to launch ChatHead intent.

--- AndroidChatHead/src/main/java/com/gbinghan/androidchathead/MainActivity.java
+++ AndroidChatHead/src/main/java/com/gbinghan/androidchathead/MainActivity.java
@@ -1,8 +1,10 @@
 package com.gbinghan.androidchathead;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChatHeadService.class);
+                intent.putExtra("title", "Hello");
+                intent.putExtra("text", "ChatHead");
+                startService(intent);
+            }
+        });
+
     }
 
 
